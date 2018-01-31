8f30f301c1c893aff87df6a90ff9d8782df59ef8
Joshua Nelson <jyn514@gmail.com>
2018-01-31 15:41:12 -0500

    generate getters and setters for jeap

--- Homework/07jeapHeap/src/Jeep.java
+++ Homework/07jeapHeap/src/Jeep.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 class Jeep {
 
     final String name;
@@ -7,5 +9,42 @@ class Jeep {
         name = givenName;
         weight = givenWeight;
         horsepower = givenHorsepower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Jeep{" +
+            "name='" + name + '\'' +
+            ", weight=" + weight +
+            ", horsepower=" + horsepower +
+            '}';
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public float getHorsepower() {
+        return horsepower;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Jeep jeep = (Jeep) o;
+        return Float.compare(jeep.getWeight(), getWeight()) == 0 &&
+            Float.compare(jeep.getHorsepower(), getHorsepower()) == 0 &&
+            Objects.equals(getName(), jeep.getName());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getName(), getWeight(), getHorsepower());
     }
 }
