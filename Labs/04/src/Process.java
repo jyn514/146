817d82c18754f399b3c91352b70e80ee1f355e0e
Joshua Nelson <jyn514@gmail.com>
2018-02-16 12:14:26 -0500

    reduce access; remove logs

--- Labs/04/src/Process.java
+++ Labs/04/src/Process.java
@@ -1,16 +1,23 @@
 
-public class Process {
-  public String name;
-  private double completionTime; // should only be modified through methods
-  public static final double MAX_PROC_TIME = 15.0;
+class Process {
+  public final String name;
+  private double completionTime; // should only be accessed through methods
 
-  public Process() {
-    name = "YouUsedTheDefaultConstructor";
+  public static final double MAX_PROC_TIME = 15.0;
+  private static final String DEFAULT_NAME = "YouUsedTheDefaultConstructor";
+
+  Process() {
+    name = DEFAULT_NAME;
     // time between 0 and 10 ** 10
     completionTime = Math.random() * MAX_PROC_TIME;
   }
   
-  public Process(String givenName, double randTime) {
+  Process(double time) {
+	  name = DEFAULT_NAME;
+	  setCompletionTime(time);
+  }
+  
+  Process(String givenName, double time) {
     name = givenName;
     completionTime = randTime;
   }
