d6a39db1c49c4992d5ba103156a08949f535be8c
Joshua Nelson <jyn514@gmail.com>
2018-02-23 12:37:33 -0500

    add lab 6 and report

--- Labs/06/ContainedWords.java
+++ Labs/06/ContainedWords.java
@@ -1,38 +1,55 @@
+/**
+ * Copyright (c) 2018 Joshua Nelson
+ * Licensed under the GPL 3
+ * See https://www.gnu.org/licenses/gpl-3.0.en.html for details
+ */
+
 import java.util.Scanner;
+import static java.util.Arrays.sort;
+import static java.util.Arrays.copyOfRange;
 
 public class ContainedWords {
 	public static void main(String[] args) {
-		System.out.println("Enter two strings to compare them, or type EOF (ctrl+D) to exit.");
+		System.out.println("Enter two strings to compare them, or type EOF"
+						   + " (ctrl+D on UNIX, ctrl+C on Windows) to exit.");
 		Scanner keyboard = new Scanner(System.in);
 		String one, two;
 		while (true) {
 			if (keyboard.hasNext()) {
 				one = keyboard.next();
-			} else return;
+			} else break;
 			if (keyboard.hasNext()) {
 				two = keyboard.next();
-			} else return;
-			System.out.println(contains(one, two));
+			} else break;
+			System.out.println(containsAllLetters(one, two));
 		}
 	}
 
+	private static boolean containsAllLetters(char[] one, char[] two) {
+		return containsAllLetters(new String(one), new String(two));
+	}
+	
 	/**
-	 * Convenience method for contains
+	 *  @param one string that must be contained
+	 *  @param two string to check against
+	 *	@return whether <code>two</code> contains all of the letters in <code>one</code>.
 	 */
-	private static boolean contains(String one, String two) {
-		return contains(one, two, false);
-	}
+	private static boolean containsAllLetters(String one, String two) {
+		if (one == null) 					return two == null;
+		if (one.equals(two)) 				return true;
+		if (one.length() > two.length()) 	return false;
+		if (one.length() == 1) 				return two.indexOf(one) > -1; // same as built-in contains
 
-	private static boolean contains(String one, String two, boolean switched) {
-		if (one == null) return two == null;
-		if (one.length() == 1) { // iterate through string two, return whether one is present
-			// avoid OOB exception   and  first character of two is one   or rest of two contains one
-			return two.length() != 0 && ((two.charAt(0) == one.charAt(0)) || contains(one, two.substring(1)));
-		}
-		// split one into component letters; this is usually run first
-		if (contains(one.substring(0, 1), two) && contains(one.substring(1), two)) return true;
-		// switched parameter is necessary to avoid infinite recursion
-		if (!switched) return contains(two, one, true);
-		return false;
+		char[] oneSorted = one.toCharArray(), twoSorted = two.toCharArray();
+		sort(oneSorted); // these return void
+		sort(twoSorted);
+
+		// find first occurence of first char in one
+		int index = new String(twoSorted).indexOf(oneSorted[0]);
+		if (index < 0) return false; // doesn't contain char
+		
+		// move to next letter in both
+		return containsAllLetters(copyOfRange(oneSorted, 1, oneSorted.length),
+								  copyOfRange(twoSorted, index + 1, twoSorted.length));
 	}
 }
