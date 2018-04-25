4b13425f1901530bd1501f016b79c572d270a2bf
Joshua Nelson <jyn514@gmail.com>
2018-04-24 21:29:57 -0400

    Discovered default methods for interfaces

--- General/src/Heap.java
+++ General/src/Heap.java
@@ -1,6 +1,8 @@
 package src;
 
-import java.lang.reflect.Array;
+import java.util.Iterator;
+
+import static java.lang.reflect.Array.newInstance;
 
 /**
  * Copyright © (2018) Joshua Nelson
@@ -9,20 +11,41 @@ import java.lang.reflect.Array;
  * but you must preserve this copyright notice and make any changes available as source code to all users.
  * Complete information available at https://www.gnu.org/licenses/gpl-3.0.en.html
  */
-public interface Heap<T extends Comparable<T>> extends BinaryTree<T>, Iterable<T> {
+public interface Heap<T extends Comparable<T>> extends Iterable<T> {
 
 	void add(T t);
-	boolean isEmpty();
 	int size();
 	T peek();
 	T pop();
 	Heap<T> clone();
 
+	default boolean isEmpty() {
+		return size() == 0;
+	}
+
+	default T[] heapSort() {
+		return Heap.heapSort(this);
+	}
+
+	default Iterator<T> iterator() {
+		return new Iterator<T>() {
+			private Heap<T> clone = Heap.this.clone();
+
+			public boolean hasNext() {
+				return !clone.isEmpty();
+			}
+
+			public T next() {
+				return clone.pop();
+			}
+		};
+	}
+
 	static <T extends Comparable<T>> T[] heapSort(Heap<T> heap) {
 		int size = heap.size();
 		if (size == 0) return null;
 		T first = heap.pop();
-		T[] result = (T[]) Array.newInstance(first.getClass(), size);
+		T[] result = (T[]) newInstance(first.getClass(), size);
 		result[0] = first;
 
 		Heap<T> clone = heap.clone();
